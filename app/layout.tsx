1788812f754a050644a82f6ad0360b0c5ce91f05
Shriyansh Bhargava <63575417+shriyanshbhargava@users.noreply.github.com>
2024-12-02 12:07:40 +0530

    changed favicon ico.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Provider } from "@/components/Provider/Provider";
 import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      {" "}
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </Head>
       <body className={inter.className}>
         <Analytics />
         <Provider>{children}</Provider>
